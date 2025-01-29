7808c8ef581c15803b06383b44367f2f0c6a55fd
Deepthi-n1 <deepthinachari@gmail.com>
2025-01-29 12:10:35 +0530

    Add files via upload

--- LAB1/PROG1.py
+++ LAB1/PROG1.py
@@ -1,11 +1,25 @@
-A=[10,89,9,56,4,80,8]
- largest=arr[0]
- smallest=arr[0]
+array = [10, 89, 9, 56, 4, 80, 8]
 
-    for num in arr:
-    	    if num > largest:
-	    	    largest = num
-	         if num < smallest:
-		         smallest = num
+largest = array[0]
+second_largest = None
+smallest = array[0]
+second_smallest = None
 
-         return largest,smallest
+for element in array:
+    if element > largest:
+        second_largest = largest
+        largest = element
+    elif second_largest is None or element > second_largest:
+        second_largest = element
+    
+    if element < smallest:
+        second_smallest = smallest
+        smallest = element
+    elif second_smallest is None or element < second_smallest:
+        second_smallest = element
+
+print("largest element:", largest)
+print("second largest element:", second_largest)
+print("smallest element:", smallest)
+print("second smallest element:", second_smallest)
+S
